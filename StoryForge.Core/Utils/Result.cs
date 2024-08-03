54a01a40880e40adc9fed1e48259b504bc96073d
Asyraf <asyzruffz@gmail.com>
2024-08-04 03:25:59 +0800

    Add Result object

---
+++ StoryForge.Core/Utils/Result.cs
@@ -0,0 +1,48 @@
+﻿namespace StoryForge.Core.Utils;
+
+public class Result
+{
+    public bool IsSuccess { get; init; }
+    public string ErrorMessage { get; init; }
+
+    private Result(bool isSuccess, string errorMessage)
+    {
+        IsSuccess = isSuccess;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result Ok() => new Result(true, string.Empty);
+    public static Result Fail(string errorMessage) => new Result(false, errorMessage);
+
+    public Result Then(Func<Result> action) =>
+        IsSuccess ? action() : Result.Fail(ErrorMessage);
+    public Result<TResult> Then<TResult>(Func<Result<TResult>> action) =>
+        IsSuccess ? action() : Result<TResult>.Fail(ErrorMessage);
+    public void Then(Action action) { if (IsSuccess) action(); }
+}
+
+public class Result<T>
+{
+    public bool IsSuccess { get; init; }
+    public string ErrorMessage { get; init; }
+
+    private T Value;
+
+    private Result(bool isSuccess, T value, string errorMessage)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        ErrorMessage = errorMessage;
+    }
+
+    public static Result<T> Ok(T value) => new Result<T>(true, value, string.Empty);
+    public static Result<T> Fail(string errorMessage) => new Result<T>(false, default!, errorMessage);
+
+    public T Or(T defaultVal) => IsSuccess ? Value : defaultVal;
+
+    public Result Then(Func<T, Result> action) =>
+        IsSuccess ? action(Value) : Result.Fail(ErrorMessage);
+    public Result<TResult> Then<TResult>(Func<T, Result<TResult>> action) =>
+        IsSuccess ? action(Value) : Result<TResult>.Fail(ErrorMessage);
+    public void Then(Action<T> action) { if (IsSuccess) action(Value); }
+}
