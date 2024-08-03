8324b2afbd0c0871a31fb6a78315e3254ae224b0
Asyraf <asyzruffz@gmail.com>
2024-08-03 11:47:31 +0800

    Add project files.

---
+++ StoryForge.Simulator/Program.cs
@@ -0,0 +1,15 @@
+﻿
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging(logging => logging.ClearProviders().AddProvider(NullLoggerProvider.Instance))
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IDataSession, DataSession>();
+        services.AddSingleton<IDataSessionFactory, DataSessionFactory>();
+
+        services.AddTomeSystem();
+        services.AddCommandService();
+        services.AddHostedService<TomeSimulator>();
+    })
+    .Build();
+
+host.Run();
