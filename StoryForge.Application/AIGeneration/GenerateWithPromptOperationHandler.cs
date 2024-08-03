c52b7d3099d52b2bb478431066a3974546ccc2e3
Asyraf <asyzruffz@gmail.com>
2024-08-04 06:00:08 +0800

    Refactor

--- StoryForge.Application/AIGeneration/GenerateWithPromptOperationHandler.cs
+++ StoryForge.Application/AIGeneration/GenerateWithPromptOperationHandler.cs
@@ -1,23 +1,22 @@
 ﻿using StoryForge.Application.Abstractions;
 using StoryForge.Core.Utils;
 
-namespace StoryForge.Application.AIGeneration
+namespace StoryForge.Application.AIGeneration;
+
+internal sealed class GenerateWithPromptOperationHandler
+    : IOperationHandler<GenerateWithPromptOperation, string>
 {
-    internal sealed class GenerateWithPromptOperationHandler
-        : IOperationHandler<GenerateWithPromptOperation, string>
+    public GenerateWithPromptOperationHandler()
     {
-        public GenerateWithPromptOperationHandler()
-        {
-            // dependency injection
-        }
+        // dependency injection
+    }
 
-        public async Task<Result<string>> Handle(GenerateWithPromptOperation request, CancellationToken cancellationToken)
-        {
-            if (string.IsNullOrWhiteSpace(request.Prompt))
-                return await Task.FromResult(Result<string>.Fail("Prompt is empty"));
+    public async Task<Result<string>> Handle(GenerateWithPromptOperation request, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrWhiteSpace(request.Prompt))
+            return await Task.FromResult(Result<string>.Fail("Prompt is empty"));
 
-            var generated = $"This is an AI genrated response from the prompt: \"{request.Prompt}\"";
-            return await Task.FromResult(Result<string>.Ok(generated));
-        }
+        var generated = $"This is an AI genrated response from the prompt: \"{request.Prompt}\"";
+        return await Task.FromResult(Result<string>.Ok(generated));
     }
 }
