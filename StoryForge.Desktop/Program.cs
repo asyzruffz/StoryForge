c9c5e92d047732188e4924c74b2ffe6c0c1b39d0
Asyraf <asyzruffz@gmail.com>
2026-01-04 17:39:50 +0800

    Add project for desktop app

---
+++ StoryForge.Desktop/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using Photino.Blazor;
+using StoryForge.Desktop.UI;
+
+namespace StoryForge.Desktop;
+
+internal class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        var builder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        builder.Services
+            .AddLogging()
+            .AddMudServices();
+
+        builder.RootComponents.Add<App>("app");
+
+        var app = builder.Build();
+
+        app.MainWindow
+            .SetUseOsDefaultSize(true)
+            .SetUseOsDefaultLocation(true)
+            .SetIconFile("favicon.ico")
+            .SetTitle("Story Forge");
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.ShowMessage("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
