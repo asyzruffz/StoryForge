4c39616a5055d1f013e20e2a2073ae49f9fc725c
Asyraf <asyzruffz@gmail.com>
2026-02-05 11:50:32 +0800

    Implement word count in summary

---
+++ StoryForge.Desktop/Utils/WordCounter.cs
@@ -0,0 +1,10 @@
+﻿namespace StoryForge.Desktop.Utils;
+
+internal static class WordCounter
+{
+    public static int WordCount(this string text)
+    {
+        var words = text.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        return words.Count();
+    }
+}
