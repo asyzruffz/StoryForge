9cd386534d41912d8ea8d74e3dc37dd99f74ea08
Asyraf <asyzruffz@gmail.com>
2026-02-11 00:56:55 +0800

    Add chracters get operation

---
+++ StoryForge.Application/Characters/GetCharacterEntriesOperation.cs
@@ -0,0 +1,26 @@
+﻿using StoryForge.Application.Abstractions;
+using StoryForge.Core.Data;
+using StoryForge.Core.Services;
+using StoryForge.Core.Utils;
+using System.Collections.Generic;
+
+namespace StoryForge.Application.Characters;
+
+public sealed record GetCharacterEntriesOperation : IOperation<IEnumerable<CharacterEntry>>;
+
+internal sealed class GetCharacterEntriesOperationHandler : IOperationHandler<GetCharacterEntriesOperation, IEnumerable<CharacterEntry>>
+{
+    private readonly IDataSession data;
+
+    public GetCharacterEntriesOperationHandler(IDataSession dataSession)
+    {
+        data = dataSession;
+    }
+
+    public async Task<Result<IEnumerable<CharacterEntry>>> Handle(GetCharacterEntriesOperation request, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+
+        return Result<IEnumerable<CharacterEntry>>.Fail("GetCharacterEntriesOperation not implemented"); //result;
+    }
+}
---
+++ StoryForge.Application/Characters/GetCharacterOperation.cs
@@ -0,0 +1,26 @@
+﻿using StoryForge.Application.Abstractions;
+using StoryForge.Core.Data;
+using StoryForge.Core.Services;
+using StoryForge.Core.Utils;
+
+namespace StoryForge.Application.Characters;
+
+public sealed record GetCharacterOperation(CharacterId Id) : IOperation<Character>;
+
+internal sealed class GetCharacterOperationHandler : IOperationHandler<GetCharacterOperation, Character>
+{
+
+    private readonly IDataSession data;
+
+    public GetCharacterOperationHandler(IDataSession dataSession)
+    {
+        data = dataSession;
+    }
+
+    public async Task<Result<Character>> Handle(GetCharacterOperation request, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+
+        return Result<Character>.Fail("GetCharacterOperation not implemented"); //result;
+    }
+}
