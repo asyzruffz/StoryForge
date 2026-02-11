3bee344eb41343c11949d639019e1cee5676ed48
Asyraf <asyzruffz@gmail.com>
2026-02-11 12:18:02 +0800

    Implement update summary operation

---
+++ StoryForge.Application/Summaries/UpdateSummaryOperation.cs
@@ -0,0 +1,26 @@
+﻿using StoryForge.Application.Abstractions;
+using StoryForge.Core.Data;
+using StoryForge.Core.Services;
+using StoryForge.Core.Utils;
+
+namespace StoryForge.Application.Summaries;
+
+public sealed record UpdateSummaryOperation(Summary Summary) : IOperation;
+
+internal sealed class UpdateSummaryOperationHandler : IOperationHandler<UpdateSummaryOperation>
+{
+    private readonly IDataSession data;
+
+    public UpdateSummaryOperationHandler(IDataSession dataSession)
+    {
+        data = dataSession;
+    }
+
+    public async Task<Result> Handle(UpdateSummaryOperation request, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        data.Summaries.Update(request.Summary);
+        data.Save();
+        return Result.Ok();
+    }
+}
