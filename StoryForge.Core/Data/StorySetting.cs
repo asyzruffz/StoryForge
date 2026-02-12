6631e49533ef25d6cac7c1a05e80b4318416a7cf
Asyraf <asyzruffz@gmail.com>
2026-02-12 23:53:15 +0800

    Add story setting category

--- StoryForge.Core/Data/StorySetting.cs
+++ StoryForge.Core/Data/StorySetting.cs
@@ -4,13 +4,14 @@ public class StorySetting
 {
     public StorySettingId Id { get; set; } = StorySettingId.Empty;
     public string Name { get; set; } = string.Empty;
+    public StorySettingCategory Category { get; set; } = StorySettingCategory.General;
     public string Description { get; set; } = string.Empty;
     public string Details { get; set; } = string.Empty;
 
     public static StorySetting New(string name) =>
         new() { Id = StorySettingId.New(), Name = name };
 
-    public StorySettingEntry ToEntry() => new StorySettingEntry(Id, Name);
+    public StorySettingEntry ToEntry() => new StorySettingEntry(Id, Name, Category);
 }
 
 public readonly record struct StorySettingId(string Value) : ITypedId
@@ -20,8 +21,14 @@ public readonly record struct StorySettingId(string Value) : ITypedId
     public static StorySettingId From(string value) => new(TypedId.From(value));
 }
 
-public record StorySettingEntry(StorySettingId Id, string Name) : IIdentifiable
+public record StorySettingEntry(StorySettingId Id, string Name, StorySettingCategory Category) : IIdentifiable
 {
     public string IdString => Id.Value;
+    public override string ToString() => Name;
+}
+
+public record StorySettingCategory(string Name)
+{
+    public static StorySettingCategory General = new("General");
     public override string ToString() => Name;
 }
