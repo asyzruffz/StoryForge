6979c1219c47135ea90c3b5500012fc29cf5c910
Asyraf <asyzruffz@gmail.com>
2026-02-12 19:03:41 +0800

    Rename WorldSetting to StorySetting

---
+++ StoryForge.Core/Data/StorySetting.cs
@@ -0,0 +1,20 @@
+﻿namespace StoryForge.Core.Data;
+
+public class StorySetting
+{
+    public StorySettingId Id { get; set; } = StorySettingId.Empty;
+    public string Name { get; set; } = string.Empty;
+}
+
+public readonly record struct StorySettingId(string Value) : ITypedId
+{
+    public static StorySettingId Empty => new(string.Empty);
+    public static StorySettingId New() => new(TypedId.New());
+    public static StorySettingId From(string value) => new(TypedId.From(value));
+}
+
+public record StorySettingEntry(StorySettingId Id, string Name) : IIdentifiable
+{
+    public string IdString => Id.Value;
+    public override string ToString() => Name;
+}
