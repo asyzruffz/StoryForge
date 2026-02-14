b69af709bdc22154134b2f8b85b7fe7f603335b9
Asyraf <asyzruffz@gmail.com>
2026-02-15 05:35:04 +0800

    Create Project class

---
+++ StoryForge.Core/Data/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace StoryForge.Core.Data;
+
+public class Project
+{
+    public ProjectId Id { get; set; } = ProjectId.Empty!;
+    public Book Book { get; set; } = default!;
+    public Author Author { get; set; } = default!;
+}
+
+public readonly record struct ProjectId(string Value) : ITypedId
+{
+    public static ProjectId Empty => new(TypedId.Empty);
+    public static ProjectId New() => new(TypedId.New());
+    public static ProjectId From(string value) => new(TypedId.From(value));
+}
---
+++ StoryForge.Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,5 @@
+﻿using StoryForge.Core.Data;
+
+namespace StoryForge.Core.Repositories;
+
+public interface IProjectRepository : IRepository<Project>, IQueryableById<Project, ProjectId>;
