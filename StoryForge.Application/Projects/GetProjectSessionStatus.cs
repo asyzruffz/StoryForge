7ba6e8a1852ca45d6b968675ca144014ce75e45a
Asyraf <asyzruffz@gmail.com>
2026-02-17 05:11:09 +0800

    Add WIP starter page

---
+++ StoryForge.Application/Projects/GetProjectSessionStatus.cs
@@ -0,0 +1,22 @@
+﻿using StoryForge.Application.Abstractions;
+using StoryForge.Core.Services;
+using StoryForge.Core.Utils;
+
+namespace StoryForge.Application.Projects;
+
+public record GetProjectSessionStatus : IOperation<bool>;
+
+internal sealed class GetProjectSessionStatusHandler : IOperationHandler<GetProjectSessionStatus, bool>
+{
+    private readonly IProjectSessionHandler projectSession;
+
+    public GetProjectSessionStatusHandler(IProjectSessionHandler projectSessionHandler)
+    {
+        projectSession = projectSessionHandler;
+    }
+
+    public Task<Result<bool>> Handle(GetProjectSessionStatus request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(Result<bool>.Ok(projectSession.IsActive));
+    }
+}
