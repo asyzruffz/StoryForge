71d0a3a7f84ba1bbe024e5a6b994c4d119b66e50
Asyraf <asyzruffz@gmail.com>
2026-02-17 01:41:23 +0800

    Refactor Option to Result converter

--- StoryForge.Core/Utils/UtilExtension.cs
+++ StoryForge.Core/Utils/UtilExtension.cs
@@ -2,8 +2,8 @@
 
 public static class UtilExtension
 {
-    public static Result<T> ToResult<T>(this Option<T> option) =>
-        option.Match(Result<T>.Ok, () => Result<T>.Fail("Data not available"));
+    public static Result<T> ToResult<T>(this Option<T> option, string? errorMsg = null) =>
+        option.Match(Result<T>.Ok, () => Result<T>.Fail(errorMsg ?? "Data not available"));
 
     public static Option<T> ToOption<T>(this Result<T> result) =>
         result.Match(Option<T>.Some, _ => Option<T>.None());
