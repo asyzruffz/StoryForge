cc98923f395b1f5617e51e688a578207192f5954
Asyraf <asyzruffz@gmail.com>
2026-02-19 03:51:04 +0800

    Work on the starter panel

---
+++ StoryForge.Desktop/Utils/StringUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace StoryForge.Desktop.Utils;
+
+internal static class StringUtils
+{
+    public static string EllipsisMiddle(this string text, int maxLength)
+    {
+        maxLength = Math.Min(maxLength, text.Length);
+        if (text.Length <= 4) return "...";
+        int startPartLength = (maxLength - 3) / 2;
+        int endPartLength = maxLength - startPartLength - 3;
+        return $"{text.Substring(0, startPartLength)}...{text.Substring(text.Length - endPartLength)}";
+    }
+}
