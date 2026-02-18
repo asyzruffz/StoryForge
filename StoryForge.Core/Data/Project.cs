2bbf68fd29ff351eb0df75324366c709f87b9908
Asyraf <asyzruffz@gmail.com>
2026-02-19 02:08:41 +0800

    Make recent projects table clickable

--- StoryForge.Core/Data/Project.cs
+++ StoryForge.Core/Data/Project.cs
@@ -5,6 +5,7 @@ public class Project
     public string FilePath { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public bool IsFavourite { get; set; } = false;
 
     public DateTime LastActiveLocal => LastActive.ToLocalTime();
 
