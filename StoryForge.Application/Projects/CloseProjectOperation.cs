ba36651d10a939f04f23182814070d784d855282
Asyraf <asyzruffz@gmail.com>
2026-02-18 08:48:02 +0800

    Add closing project

---
+++ StoryForge.Application/Projects/CloseProjectOperation.cs
@@ -0,0 +1,24 @@
+﻿using StoryForge.Application.Abstractions;
+using StoryForge.Core.Services;
+using StoryForge.Core.Utils;
+
+namespace StoryForge.Application.Projects;
+
+public sealed record CloseProjectOperation : IOperation;
+
+internal sealed class CloseProjectOperationHandler : IOperationHandler<CloseProjectOperation>
+{
+    private readonly IProjectSessionHandler projectSession;
+
+    public CloseProjectOperationHandler(IProjectSessionHandler projectSessionHandler)
+    {
+        projectSession = projectSessionHandler;
+    }
+
+    public async Task<Result> Handle(CloseProjectOperation request, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        projectSession.StopSession();
+        return Result.Ok();
+    }
+}
