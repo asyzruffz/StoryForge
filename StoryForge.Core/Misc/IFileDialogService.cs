847ae09749d23e3f03b9dbbf56af32b4f2f50794
Asyraf <asyzruffz@gmail.com>
2026-04-14 11:27:47 +0800

    Add FileDialogService

---
+++ StoryForge.Core/Misc/IFileDialogService.cs
@@ -0,0 +1,7 @@
+﻿namespace StoryForge.Core.Misc;
+
+public interface IFileDialogService
+{
+    Task<string?> ShowOpenFileAsync(string title, (string Name, string Extension)? filter = null);
+    Task<string?> ShowSaveFileAsync(string title, (string Name, string Extension)? filter = null);
+}
---
+++ StoryForge.Desktop/Services/FileDialogService.cs
@@ -0,0 +1,37 @@
+﻿using Photino.Blazor;
+using Photino.NET;
+using StoryForge.Core.Misc;
+
+namespace StoryForge.Desktop.Services;
+
+internal class FileDialogService : IFileDialogService
+{
+    private readonly PhotinoWindow window;
+
+    public FileDialogService(PhotinoBlazorApp app)
+    {
+        window = app.MainWindow;
+    }
+
+    public async Task<string?> ShowOpenFileAsync(string title, (string Name, string Extension)? filter = null)
+    {
+        // TODO: Save the last used directory and use it as the initial directory for the file dialog
+
+        var filePaths = await window.ShowOpenFileAsync(title, filters: ProcessFilter(filter))
+            .ConfigureAwait(false);
+        return filePaths?.FirstOrDefault();
+    }
+
+    public async Task<string?> ShowSaveFileAsync(string title, (string Name, string Extension)? filter = null)
+    {
+        var filePath = await window.ShowSaveFileAsync(title, filters: ProcessFilter(filter))
+            .ConfigureAwait(false);
+        return filePath;
+    }
+
+    (string, string[])[]? ProcessFilter((string Name, string Extension)? filter)
+    {
+        if (filter == null) return null;
+        return [(filter.Value.Name, [filter.Value.Extension])];
+    }
+}
